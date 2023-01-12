e6c077714666c8d6fd92c4ef467e22038d866b41
Ruiyan Liang <Jliang@lcs.on.ca>
2023-01-12 11:45:29 -0500

    Initial Commit

---
+++ LayoutExercises/LayoutExercisesApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutExercisesApp.swift
+//  LayoutExercises
+//
+//  Created by dyhidrogen monoxide on 2023/1/12.
+//
+
+import SwiftUI
+
+@main
+struct LayoutExercisesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
